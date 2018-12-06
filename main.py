f746e569723a753d5e27ec7b19b85a92e851ad30
Nguyễn Đức Hùng <ndgnuh99@gmail.com>
2018-12-06 20:46:26 +0700

    Add main.py (example) file

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import sympy as sp
+from ode import euler, rk, trapezoidal
+
+"""
+  voi phuong phap hien dung numpy de tao ham f
+"""
+
+f = lambda x,y: np.sin(y)
+x0 = 0; y0 = 1; x = 4; steps=10
+[xk, yk] = euler.explicit(f, x0, y0, x, steps)
+print("xk:", xk)
+print("yk:", yk)
+
+
+"""
+  voi phuong phap hien an dung sumpy de tao ham f
+"""
+
+f = lambda x,y: sp.sin(y)
+x0 = 0; y0 = 1; x = 5; steps=2
+[xk, yk] = euler.implicit(f, x0, y0, x, steps)
