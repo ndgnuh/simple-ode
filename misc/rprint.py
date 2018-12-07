e8ed85dace4834dff8c177858f1e95b6df2761a9
Nguyễn Đức Hùng <ndgnuh99@gmail.com>
2018-12-07 16:45:23 +0700

    Implement Implicit Euler for system

--- misc/rprint.py
+++ misc/rprint.py
@@ -1,30 +1,30 @@
 from numpy import round, asarray
 
-def result(xk, yk):
+def result(xk, yk, dec=6):
   print("-"*30)
   if(type(yk[0]) == type([])):
-    _system_result(xk, yk)
+    _system_result(xk, yk, dec)
     return 0
   if(type(yk[0]) == type(asarray([]))):
-    _system_result(xk, yk)
+    _system_result(xk, yk, dec)
     return 0
-  _scalar_result(xk, yk)
+  _scalar_result(xk, yk, dec)
 
-def _system_result(xk, yk):
+def _system_result(xk, yk, dec):
   cols = len(yk[0])
-  print("xk".ljust(10), end="")
+  print("xk".ljust(dec+5), end="")
   for col in range(0, cols):
-    print(("y"+ str(col) +"k").ljust(10), end="", sep="")
-  print("\n"+"-"*(cols+1)*10)
+    print(("y"+ str(col) +"k").ljust(dec+5), end="", sep="")
+  print("\n"+"-"*(cols+1)*(dec+5))
   for row in range(0, len(xk)):
-    print(str(round(xk[row],4)).ljust(10), end="")
+    print(str(round(xk[row],dec)).ljust(dec+5), end="")
     for col in range(0, len(yk[0])):
-      print(str(round(yk[row][col],4)).ljust(10), end="")
+      print(str(round(yk[row][col],dec)).ljust(dec+5), end="")
     print("")
 
-def _scalar_result(xk, yk):
+def _scalar_result(xk, yk, dec=6):
   print("xk".ljust(10), "yk".ljust(10))
   print("-"*30)
   for i in range(0, len(xk)):
-    print(str(round(xk[i],4)).ljust(10), end="")
-    print(str(round(yk[i],4)).ljust(10))
+    print(str(round(xk[i],dec)).ljust(dec+5), end="")
+    print(str(round(yk[i],dec)).ljust(dec+5))
--- ode/euler.py
+++ ode/euler.py
@@ -1,20 +1,7 @@
-import numpy as np 
-import inspect
-import matplotlib.pyplot as plt
-from sympy import Symbol, lambdify
-from scipy.optimize import newton
+import numpy as np
 
-def explicit(f,xk,yk,x,stepnum,epsilon=0):
-  print("\n\nExplicit Euler method:")
-  if(stepnum == 0):
-    print("Invalid number of steps. Exiting...")
-    return 1
-  h = (x-xk)/stepnum
-  i = 0
-  xks=[xk]
-  yks=[yk]
-  print("step size h = ", h)
-  print("y' =", inspect.getsource(f), end="\n")
+def _explicit_single(f,xk,yk,x,stepnum,epsilon=0):
+  xks=[xk];  yks=[yk]; h = (x - xk)/stepnum
   for i in range(0, stepnum):
     yk = yk + h * f(xk, yk)
     xk+= h
@@ -22,40 +9,68 @@ def explicit(f,xk,yk,x,stepnum,epsilon=0):
     xks.append(xk)
     yks.append(yk)
   print("y(", xk,") = ", yk)
-  return [np.asarray(xks),np.asarray(yks)]
+  return [xks, yks]
 
-def implicit(f,xk,yk,x,stepnum,epsilon=0):
-  try:
-    print("\n\nImplicit Euler method:")
-    if(stepnum == 0):
-      print("Invalid number of steps. Exiting...")
-      return 1
-    xks=[xk]
-    yks=[yk]
-    h = (x-xk)/stepnum
-    print("step h =", h)
-    print("y' =", inspect.getsource(f), end="\n")
-    y  = Symbol("y") #y ~ y(k+1), symbolic for solving
-    for i in range(0, stepnum):
-      eulerEq = h * f(xk, y) + yk - y
-      eulerEq = lambdify(y, eulerEq) 
-      yk = newton(eulerEq, xk)
-      xk = xk + h
-      xks.append(xk)
-      yks.append(yk)
-    print("y(", xk, ") = ", yk)
-    return [np.asarray(xks), np.asarray(yks)]
-  except RuntimeError:
-    print("Unable to solve, failed to converge")
-    return [np.asarray(xks), np.asarray(yks)]
-
-def explicit_syseq(f, xk, yk, x, stepnum):
+def _explicit_syseq(f, xk, yk, x, stepnum):
   h = (x-xk)/stepnum
   if(h == 0):
     print("invalid stepsize")
     return 0
+  print("step size h = ", h)
   xks = [xk]; yks=[yk]
   for i in range(0, stepnum):
     yk =np.asarray(yk) + h*np.asarray(f(xk,yk))
     xk += h; xks.append(xk); yks.append(yk)
+  return [xks, yks]
+
+def explicit(f, xk, yk, x, stepnum):
+  print("Explicit Euler method:")
+  if(stepnum == 0):
+    print("Invalid step num")
+    return 0
+  if((type(yk) == type([])) or (type(yk) == type(np.asarray([])))):
+    [xks, yks] = _explicit_syseq(f, xk, yk, x, stepnum)
+  else:
+    [xks, yks] = _explicit_single(f, xk, yk, x, stepnum)
+  return [np.asarray(xks), np.asarray(yks)]
+
+
+def implicit(f,xk,yk,x,stepnum,iter=50):
+  print("Explicit Euler method:")
+  if(stepnum == 0):
+    print("Invalid step num")
+    return 0
+  if((type(yk) == type([])) or (type(yk) == type(np.asarray([])))):
+    [xks, yks] = _implicit_syseq(f, xk, yk, x, stepnum, iter)
+  else:
+    [xks, yks] = _implicit_single(f, xk, yk, x, stepnum, iter)
+  return [np.asarray(xks), np.asarray(yks)]
+
+
+def _implicit_syseq(f,xk,yk,x,stepnum,iter=50):
+  xks=[xk];  yks=[yk];  
+  h = (x-xk)/stepnum;
+  print("step h =", h)
+  for i in range(0, stepnum):
+    xk += h
+    ybar = yk + h*np.asarray(f(xk, yk))
+    for j in range(0, iter):
+      ybar = yk + h*np.asarray(f(xk, ybar))
+    yk = yk + h*np.asarray(f(xk, ybar))
+    xks.append(xk); yks.append(yk)
+  return [xks, yks]
+
+
+def _implicit_single(f,xk,yk,x,stepnum, iter=50):
+  xks=[xk];  yks=[yk];  
+  h = (x-xk)/stepnum; 
+  print("step h =", h)
+  for i in range(0, stepnum):
+    xk += h;
+    ybar = yk + h*f(xk, yk)
+    for j in range(0, iter):
+      ybar = yk + h*f(xk, ybar)
+    yk = yk + h*f(xk, ybar)
+    xks.append(xk); yks.append(yk)
+  print("y(", xk, ") = ", yk)
   return [xks, yks]
