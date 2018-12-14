5172996f04291de0738a1699670eaffa22f29488
Nguyễn Đức Hùng <ndgnuh99@gmail.com>
2018-12-15 00:15:23 +0700

    assembling ab

--- ode/adams/adams-b.py
+++ ode/adams/adams-b.py
@@ -21,7 +21,6 @@ def gen_dfi(s):
 [p, dfi] = gen_dfi(s)
 
 
-print(p, dfi)
 
 fact_arr = gen_factarr(s)
 mat = gen_comb_matrix(s, fact_arr)
@@ -30,11 +29,29 @@ ficoef = dfi.T.dot(mat)
 print("Công thức AB-", s, ". Các hệ số của fi (i từ 0 tới ", s-1,"):", sep="")
 print(ficoef)
 
-def test(x, y):
-  return x*y
 
-def gen_bashforth():
-  return lambda x: test(x,x)
+f = lambda x, y: y
+x0 = 0; y0 = 1
+tks = [y0]
+fis = [f(x0, y0)]
+yk = y0
+xk = x0
+stepnum = 20; x = 4
+h = (x - xk)/stepnum 
+for i in range(1, stepnum):
+  yks = np.array([yk])
+  xks = np.array([xk])
+  if(i < s):
+    [p, dfi] = gen_dfi(i)
+    fact_arr = gen_factarr(i)
+    mat = gen_comb_matrix(i, fact_arr)
+    ficoef = dfi.T.dot(mat)
+  # if(i > s):
+  yk = yk + h*ficoef.T.dot(fis)
+  xk = xk + h
+  fis = np.append(fis, f(xk, yk))[-(s-1):]
+  yks = np.append(yks, [yk])
+  xks = np.append(xks, xk)
 
-f = gen_bashforth()
-print(f(3))
+print(xks, yks)
+print(ficoef)
