f330c61318d3bcc32cc15c417e8946a5060b3ded
Nguyễn Đức Hùng <ndgnuh99@gmail.com>
2018-12-21 12:57:29 +0700

    wip error eval

--- misc/error_eval.py
+++ misc/error_eval.py
@@ -1,12 +1,24 @@
 import numpy as np 
+import sys 
+sys.path.append("/home/hung/Projects/simple-ode/")
 from ode import schemes, euler, rk, trapezoidal
 
 def ex_eval(f, x0, y0, x, stepnum, exf, div=4):
   cur_stepnum = stepnum
-  x = []; y = []
+  xx = []; yy = []
+  er = []
   dy = np.array([])
   for i in range(0, div):
-    [xk, yk] = schemes.explicit(f, x0, y0, x, stepnum, exf)
-    cur_stepnum = cur_stepnum / 2
-    x.append(xk); y.append(yk)
-  print(y)
+    [xk, yk] = schemes.explicit(f, x0, y0, x, cur_stepnum, exf)
+    cur_stepnum = int(cur_stepnum * 2)
+    xx.append(xk); yy.append(yk)
+  for i in range(0, stepnum):
+    for j in range(1, div):
+      yy[j] = np.delete(yy[j], i)
+      xx[j] = np.delete(xx[j], i)
+  print(np.array(yy))
+
+f = lambda x, y: 1 - y
+x0 = 0; y0 = 0
+x = 3; stepnum = 9
+ex_eval(f, x0, y0, x, stepnum, rk.erk3)
