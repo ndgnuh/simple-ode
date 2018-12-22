33b6174fa96cfcd4cb392c1f263c3cca163b435a
Nguyễn Đức Hùng <ndgnuh99@gmail.com>
2018-12-22 19:54:42 +0700

    test

--- ode/adams/am.py
+++ ode/adams/am.py
@@ -8,7 +8,7 @@ from helper import fast_comb
 def gen_comb_matrix(s, fact_arr):
   mat = np.array([[0]*s]*s)
   for i in range(0,s):
-    sgn = 1 if (i % 2 == 0) else -1
+    sgn = 1 #if (i % 2 == 0) else -1
     for j in range(0,i+1):
       mat[i][j] = sgn*fast_comb(j, i, fact_arr)
       sgn=-sgn
@@ -46,15 +46,18 @@ def gen_comb_matrix(s, fact_arr):
 def ab_builder(s):
   dfi_coef_1 = np.array([1, 0])
   dfi_coef_2 = [1]; fact_arr = [1]
+  sgn = -1
   for i in range(1, s):
     fact_arr.append(fact_arr[i-1] * i)
     int_dfi_coef_1 = np.polyint(dfi_coef_1)
-    dfi_coef_2.append((np.polyval(int_dfi_coef_1, s) - np.polyval(int_dfi_coef_1, s-1))/fact_arr[i-1])
+    dfi_coef_2.append(sgn*(np.polyval(int_dfi_coef_1, 1) - np.polyval(int_dfi_coef_1, 0))/fact_arr[i-1])
     dfi_coef_1 = np.polymul(dfi_coef_1, [1, i])
+    sgn = -sgn
   dfi_coef_2 = np.asarray(dfi_coef_2)
   mat = gen_comb_matrix(s, fact_arr)
-  print(mat)
-  ficoef = np.flip(dfi_coef_2).T.dot(mat)
+  for i in range(0, s):
+    mat[i] = np.flip(mat[i])
+  ficoef = dfi_coef_2.T.dot(mat)
   print("ficoef of s =", s, ":", ficoef)
   def adam(f, xks, yks, h):
     xks = xks[-s:]; yks = yks[-s:]
